ffc284ca642d4b9309162f3eb7495072d90491f7
AK <adrian.kloc47@gmail.com>
2020-07-26 02:10:09 +0200

    CSS and JS modifications
    - modificated buttons positions and images
    - modificated class injection logic
    - changed tasks id generation logic

--- app.js
+++ app.js
@@ -1,51 +1,91 @@
 // global variables
-const TASK_ARRAY = [];
+const TASK_ARRAY = {};
+const TRASHED_TASKS = {};
+const DONE_TASKS = {};
 
-// queries
+// DESKTOP SCRIPTS
+
+//queries
 const startAddTask = document.querySelector('#add-task');
 const acceptAddTask = document.querySelector('#accept');
 const declineAddTask = document.querySelector('#decline');
 const backdrop = document.querySelector('#backdrop');
 const taskList = document.querySelector('.task-list');
+const search = document.querySelector('.search');
+const searchBtn = document.querySelector('.search-btn');
 
 // functions
-const createTaskDiv = (text, type) => {
+const createTaskDiv = (id, text, type) => {
   const taskDiv = document.createElement('div');
   const textDiv = document.createElement('div');
   const taskText = document.createElement('p');
-  const taskTopbar = document.createElement('div');
   const typeBadge = document.createElement('div');
   const taskType = document.createElement('p');
+  const btnBox = document.createElement('div');
   const deleteBtn = document.createElement('button');
+  const doneBtn = document.createElement('button');
   taskText.innerText = text;
   taskType.innerText = type;
-  deleteBtn.innerHTML = '<img src="assets/trash_icon.png" height="15px" width="15px"></img>';
   taskDiv.classList.add('task-div');
   textDiv.classList.add('task-text-div');
   typeBadge.classList.add('type-badge-div');
-  deleteBtn.classList.add('delete-task-btn');
-  taskTopbar.classList.add('task-topbar');
-  taskDiv.setAttribute('id', `${generateId()}`)
+  btnBox.classList.add('task-button-box');
+  deleteBtn.classList.add('task-btn', 'delete-task-btn');
+  doneBtn.classList.add('task-btn', 'done-task-btn');
+  taskDiv.setAttribute('id', `${id}`)
   typeBadge.appendChild(taskType);
   textDiv.appendChild(taskText);
-  taskTopbar.appendChild(typeBadge);
-  taskTopbar.appendChild(deleteBtn);
-  taskDiv.appendChild(taskTopbar);
+  taskDiv.appendChild(typeBadge);
   taskDiv.appendChild(textDiv);
+  btnBox.appendChild(doneBtn);
+  btnBox.appendChild(deleteBtn);
+  taskDiv.appendChild(btnBox);
   taskList.appendChild(taskDiv);
-  deleteBtn.addEventListener('click', deleteTaskDiv);
+  doneBtn.addEventListener('click', (e) => {
+    doneTaskDiv(e);
+  })
+  deleteBtn.addEventListener('click', (e) => {
+    const task = e.target.closest('.task-div');
+    task.classList.toggle('task-div-deleted');
+    setTimeout(() => {
+      deleteTaskDiv(e);
+    }, 500);
+  });
 }
 const deleteTaskDiv = (element) => {
-  const task = element.target.parentNode.parentNode.parentNode;
+  const task = element.target.closest('.task-div');
+  const taskId = task.id;
+  const taskType = task.children[0].innerText;
+  const taskText = task.children[1].innerText;
+  TRASHED_TASKS[`${taskId}`] = {taskType, taskText};
   task.remove();
 }
-const generateId = () => {
+const doneTaskDiv = (element) => {
+  const task = element.target.closest('.task-div');
+  const taskId = task.id;
+  const taskType = task.children[0].innerText;
+  const taskText = task.children[1].innerText;
+  DONE_TASKS[`${taskId}`] = {taskType, taskText};
+}
+//const doneTaskDiv = () => {}
+const generateId = (type) => {
   const chars = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
   const id = [];
+  switch(type){
+    case 'task-div':
+      id.push('task-div_');
+      break;
+  }
   for (let i = 0; i < 8; i++) {
     id.push(chars[Math.floor((Math.random() * 35) + 0)]);
   }
   return id.join('');
+}
+const searchHandler = () => {
+  TASK_ARRAY.forEach(item => {
+    console.log(item.taskText);
+    console.log(item.taskType);
+  })
 }
 // listeners
 startAddTask.addEventListener('click', () => {
@@ -59,14 +99,41 @@ declineAddTask.addEventListener('click', () => {
   backdrop.classList.toggle('modal-backdrop-off');
 })
 acceptAddTask.addEventListener('click', () => {
-  let taskText = document.querySelector('#task-text').value;
+  const taskId = generateId('task-div');
+  const taskText = document.querySelector('#task-text').value;
   const taskType = document.querySelector('input[name="task-type"]:checked').value;
   if(taskText){
-    TASK_ARRAY.push({taskText, taskType});
-    createTaskDiv(taskText, taskType);
+    createTaskDiv(taskId, taskText, taskType);
+    TASK_ARRAY[`${taskId}`] = {taskType, taskText};
     document.querySelector('#task-text').value = '';
-    document.querySelector('input[name="task-type"][value="general"]').checked = true;
+    document.querySelector('input[name="task-type"][value="General"]').checked = true;
     backdrop.classList = "";
     backdrop.classList.toggle('modal-backdrop-off');
   }
 })
+searchBtn.addEventListener('click', searchHandler);
+
+// MOBILE SCRIPTS
+
+//queries
+const filter = document.querySelector('.filter');
+const mobileSearchBtn = document.querySelector('.filter-search-btn');
+// functions
+
+
+// listeners
+filter.addEventListener('click', () => {
+  const filterList = document.querySelector('.filter-list-off');
+  if(filterList.classList.contains('filter-list-off')){
+    filterList.classList.toggle('filter-list-on');
+  } else if(filterList.classList.contains('filter-list-on')){
+    filterList.classList.toggle('filter-list-on');
+  }
+  if(filterList.classList.contains('filter-list-on')){
+    filter.innerHTML = '<span>↑Filter↑</span>';
+  } else {
+    filter.innerHTML = '<span>↓Filter↓</span>';
+  }
+})
+mobileSearchBtn.addEventListener('click', searchHandler);
+
